bf7d22c652348c6adb0adf87c3c0c43a0d63bed1
Cali Weber <calista.weber1@gmail.com>
2023-07-25 12:07:52 -0700

    Finished exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string FavSubject = Console.ReadLine();
+           
+            switch(FavSubject.ToUpper())
+            {
+                case "Science":
+                    Console.WriteLine("Ok, your favorite subject is: Science");
+                    break;
+                case "Math":
+                    Console.WriteLine("Ok, your favorite subject is: Math");
+                    break;
+                case "English":
+                    Console.WriteLine("Ok, your favorite subject is: English");
+                    break;
+                case "Home Economics":
+                    Console.WriteLine("Ok, your favorite subject is: Home Economics");
+                    break;
+                case "P.E":
+                    Console.WriteLine("Ok, your favorite subject is: P.E");
+                    break;
+                case "History":
+                    Console.WriteLine("Ok, your favorite subject is: History");
+                    break;
+                default: Console.WriteLine("Ok, this subject was not included");
+                    break;
+            }
         }
     }
 }
